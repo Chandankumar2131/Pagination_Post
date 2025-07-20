86f543edf41250478885a2fb96d084eb124a7847
Chandan Kumar <chandansinghrkt123@gmail.com>
2025-07-21 00:51:46 +0530

    Added blogDetail component

--- src/components/PostData.jsx
+++ src/components/PostData.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import DataContext from '../contextApp/ContextApp';
 import './PostData.css'; // make sure this path is correct
+import BlogDetails from './BlogDetails';
 
 export default function PostData() {
   const { posts, loading } = useContext(DataContext);
@@ -15,27 +16,7 @@ export default function PostData() {
         </div>
       ) : (
         posts.map((post) => (
-          <div key={post.id} className="post-card">
-            <div className="post-title">
-              <p>{post.title}</p>
-            </div>
-
-            <div className="post-meta">
-              By <span>{post.author}</span> on <span>{post.category}</span>
-            </div>
-
-            <div className="post-meta">Posted on {post.date}</div>
-
-            <div className="post-content">
-              <p>{post.content}</p>
-            </div>
-
-            <div className="post-tags">
-              {post.tags.map((tag, index) => (
-                <span key={index}>#{tag}</span>
-              ))}
-            </div>
-          </div>
+         <BlogDetails key={post.id} post={post}/>
         ))
       )}
     </div>
--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,55 +1,71 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { useLocation, useNavigation } from 'react-router-dom';
 import DataContext from '../contextApp/ContextApp';
 import Header from '../components/Header';
 
 export default function BlogsPage() {
-    const[blog,setBlog]= useState(null);
-    const[reletedBlogs,setReletedBlogs]=useState([]);
-    const location=useLocation();
-    const navigation= useNavigation();
+    const [blog, setBlog] = useState(null);
+    const [reletedBlogs, setReletedBlogs] = useState([]);
+    const location = useLocation();
+    const navigation = useNavigation();
 
-    const{setLoading,loading}= useContext(DataContext);
-const blogId = location.pathname.split("/").at(-1)
+    const { setLoading, loading } = useContext(DataContext);
+    const blogId = location.pathname.split("/").at(-1);
 
     async function fetchRelatedBlogs() {
-        const baseUrl='https://codehelp-apis.vercel.app/api/get-blogs';
-setLoading(true);
-let url = `${baseUrl}?blogId=${blogId}`;
-try {
-    const res = await fetch(url);
-    const data = await res.json();
-    setBlog(data.blog);
-    setReletedBlogs(data.reletedBlogs);
-} catch (error) {
-    console.log("Error");
-    setBlog(null);
-    setReletedBlogs([]);
-}
-setLoading(false)
+        const baseUrl = 'https://codehelp-apis.vercel.app/api/get-blogs';
+        setLoading(true);
+        let url = `${baseUrl}?blogId=${blogId}`;
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setBlog(data.blog);
+            setReletedBlogs(data.reletedBlogs);
+        } catch (error) {
+            console.log("Error");
+            setBlog(null);
+            setReletedBlogs([]);
+        }
+        setLoading(false);
     }
-    useEffect(()=>{
-        if(blogId){
+
+    useEffect(() => {
+        if (blogId) {
             fetchRelatedBlogs();
         }
-    },[location.pathname])
-  return (
-    <div>
-<Header/>
-<div>
-    <button onClick={()=>navigation(-1)}>
-        Back
-    </button>
-</div>
-{
-    loading ?(<div>
-         <p>Loading...</p>
-    </div> ):
-    blog?
-    (<BlogDetails post={blog}/>):
-    
-    (<div><p>No-Blog Foumd</p></div>)
-}
-    </div>
-  )
+    }, [location.pathname]);
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <button onClick={() => navigation(-1)}>
+                    Back
+                </button>
+            </div>
+            {
+                loading ? (
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                ) : blog ? (
+                    <div>
+                        <BlogDetails post={blog} />
+                        <h2>Related Blogs</h2>
+                        {
+                            reletedBlogs.map((post) => (
+                                <div key={post.id}>
+                                    <BlogDetails post={post} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                ) : (
+                    <div>
+                        <p>No Blog Found</p>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
